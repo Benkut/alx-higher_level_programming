2701eaf0766013e1c43aa7cd0ef5a65e1a432589
Benkut <ben1dot@yahoo.com>
2023-04-12 20:27:13 -0700

    update

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -7,8 +7,8 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """Initialize a new Student.
-        
-        Args:
+
+	Args:
             first_name (str): The first name of the student.
             last_name (str): The last name of the student.
             age (int): The age of the student.
@@ -19,11 +19,11 @@ class Student:
 
     def to_json(self, attrs=None):
         """Get a dictionary representation of the Student.
-        
-        If attrs is a list of strings, represents only those attributes
+
+	If attrs is a list of strings, represents only those attributes
         included in the list.
-       
-       Args:
+
+	Args:
             attrs (list): (Optional) The attributes to represent.
         """
         if (type(attrs) == list and
@@ -33,8 +33,8 @@ class Student:
 
     def reload_from_json(self, json):
         """Replace all attributes of the Student.
-       
-       Args:
+
+	Args:
             json (dict): The key/value pairs to replace attributes with.
         """
         for k, v in json.items():
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -7,8 +7,8 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """Initialize a new Student.
-        
-        Args:
+
+	Args:
             first_name (str): The first name of the student.
             last_name (str): The last name of the student.
             age (int): The age of the student.
